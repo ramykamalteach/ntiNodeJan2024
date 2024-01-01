099ce571e29ab4e67145915b8d53d2785c7bb8bd
ramy.kamal.teachx <76243913+ramyyat@users.noreply.github.com>
2024-01-01 22:12:24 +0200

    first

---
+++ app.js
@@ -0,0 +1,20 @@
+// variables
+
+x = 10.357;
+y = "hi everyone";
+z = true;   // boolean true false
+
+var a = 11;
+let b = "good night";
+
+a = 33;
+a = "other data";
+
+// contants
+const w = "get the value";
+//w = "other";   error
+
+
+
+
+/* console.log("Hello !!!", "first Session...."); */
